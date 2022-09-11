83f30a9a528476d2df481daf857c4c4b8bbc0f10
TomasVilk <tomykas1998@gmail.com>
2022-09-11 15:20:23 +0300

    Updates

---
+++ precache-manifest.6327c420649ac9049ef6a41c1884c84f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "67ca057faa4352c6d9ee",
+    "url": "/dev-task/static/css/main.ea96f7ac.chunk.css"
+  },
+  {
+    "revision": "67ca057faa4352c6d9ee",
+    "url": "/dev-task/static/js/main.67ca057f.chunk.js"
+  },
+  {
+    "revision": "1d6d304d6d0c8d58f3b8",
+    "url": "/dev-task/static/js/1.1d6d304d.chunk.js"
+  },
+  {
+    "revision": "d2ff1dce13611674bce7",
+    "url": "/dev-task/static/js/runtime~main.d2ff1dce.js"
+  },
+  {
+    "revision": "9a62937958d6c236a67b195d5243080a",
+    "url": "/dev-task/index.html"
+  }
+];
